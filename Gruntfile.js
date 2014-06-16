ba5cae5c879e4ce2bba72f527905100f4b110c09
Alex <github@rcnow.ru>
2014-06-16 17:23:33 +0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,47 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        //Meta
+        meta:{
+            banner: '/*!\n' +
+                ' * <%= pkg.name %> <%= pkg.description %> || (<%= pkg.homepage %>)\n' +
+                ' * Version - <%= pkg.version %>\n' +
+                ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                ' * License  <%= pkg.license %> (<%= pkg.license.url %>) \n' +
+                ' */\n'
+        },
+        // Task configuration
+        usebanner: {
+            options: {
+                position: 'top',
+                banner: '<%= meta.banner %>'
+            },
+            files: {
+                src: 'dist/css/*.css'
+            }
+        },
         sass: {
             dist: {
                 options: {
                     style: 'expanded',
+                    sourcemap: true,
                     noCache: true
                 },
                 files: {
-                    'build/css/himmel.css' : 'himmel.sass'
+                    'dist/css/himmel.css' : 'src/himmel.sass'
                 }
             }
         },
         cssmin : {
-            options: {
-                    banner: '/* Himmel minified css file  */'
-                },
             css:{
-                src: 'build/css/himmel.css',
-                dest: 'build/css/himmel.min.css'
+                src: 'dist/css/himmel.css',
+                dest: 'dist/css/himmel.min.css'
             }
         },
         watch: {
             css: {
                 files: '**/*.sass',
-                tasks: ['sass','cssmin']
+                tasks: ['sass','cssmin','usebanner']
             }
         }
 });
@@ -32,6 +49,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-banner');
 
+    //Task
     grunt.registerTask('default',['watch']);
 };
